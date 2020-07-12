ad583653dfda016c8372906434dee2addbe43ddf
Milo Fultz <milopfultz@gmail.com>
2020-07-12 00:34:38 -0700

    Working but ugly program

--- gunfight.py
+++ gunfight.py
@@ -1,65 +1,187 @@
 #Gunfight Game using Blessed
 
-# Loader - Find any config files and load them into memory
-# 	IN: file
-# 	OUT: dictionary
-def loader(fp):
-	# look for config file at filepath
-	# open file at file path
-	# dump file into a dictionary of it's contents
-	# return the new dictionary
+import json
+import random
+import string
+from sys import exit
+from time import time as get_time
+from time import sleep
+
+from blessed import Terminal
+
+TERM = Terminal()
+
+width = TERM.width
+height = TERM.height
+
+def config_loader(fp):
+	with open(fp, 'r') as f:
+		config_dict = json.load(f)
+	hs_list = config_dict['high_scores']
+	badguy_list = config_dict['bad_guys']
 	return hs_list, badguy_list
 
-# Draw - the program that initiates game sequence
-# 	IN: 
-# 	OUT: 
-def draw():
-	# get current time when program starts
-	# wait for user to press button
-	# get time of when user presses button
-	# subtract start time from button press time
-	# return the time
+
+def press_any_key():
+	with TERM.cbreak(), TERM.hidden_cursor():
+		inp = TERM.inkey()
+
+
+def speed_test(comp_speed):
+	alph = random.choice(string.ascii_lowercase)
+	with TERM.hidden_cursor():
+		print(TERM.clear)
+		random_time = random.uniform(1.5, 2.5)
+		sleep(1.5)
+		center_text('Get ready!')
+
+		sleep(1.5)
+			
+		print(TERM.clear)
+		sleep(random_time)
+
+		center_text(f'DRAW! ---- {alph}') 
+	
+	start_time = get_time()
+	
+	with TERM.cbreak():
+		val = ''
+		while val != alph:
+			val = TERM.inkey(timeout=comp_speed)
+			if not val:
+				return comp_speed
+
+	end_time = get_time()
+	user_speed = end_time - start_time
+
 	return user_speed
 
-# Timer - Sees if you beat the computer in time
-# 	IN: none
-# 	OUT: float, time
-def get_time():
-	# get current time in seconds since epoch
-	# return that time
-	return current_time
 
-# Gunshot display - show a blink on the screen like a gunshot after game is over
-# 	IN: none
-# 	OUT: none (display)
+def center_text(strng):
+	with TERM.location(y=height//2):
+			print(TERM.center(
+			TERM.bright_white + f'{strng}' + TERM.normal)) 
+
 def gunshot_display():
+	color_list = [TERM.on_bright_white, TERM.on_white, TERM.on_bright_black, TERM.on_black]
+	for i in range(4):
+		print(color_list[i], TERM.clear)
+		sleep(.06)
 	pass
 
+
 # Checker - Compares your time vs. the computer
-# 	IN: float(s), time in seconds
-# 	OUT: bool
 def checker(user_speed, comp_speed):
-	# check to see if user's speed is less than computer speed
-	win = True
-	# return the result
+	win = False
+	if user_speed < comp_speed:
+		win = True
 	return win
 
-# Leveler - Ups the level when you beat that level
-# 	IN: int, list index
-# 	OUT: str, name; float, time
-def leveler(opp_list, prev_level):
-	# check list for index
-	# unpack values of tuple found at next element in sequence
-	# return new values
-	return new_name, new_time
+
+def points(user_speed, comp_speed, level):
+	points = (1/comp_speed * (comp_speed + level)/user_speed) * 1000
+	return round(points)
+
 
 # Score Saver - Checks the last score and if it's good enough, saves it to the high score file
-# 	IN: str, name; float, time
-# 	OUT: dict
-def score_saver(name, time, hs_list):
-	# compare time input against each element other in high score list
-	# If user score is worse than current, insert it after current
-	# Remove last element in high score list
-	new_hs_list = []
-	# Return new list
-	return new_hs_list
+def high_score_saver(fp, hs_list, badguy_list, name, score):
+	new_hs = False
+
+	for rank, (old_name, old_high_score) in enumerate(hs_list):
+		if score >= old_high_score:
+			new_hs = True
+			hs_list.insert(rank, [name, score])
+			if len(hs_list) > 3:
+				hs_list = hs_list[0:2]
+			break
+
+	if new_hs:
+		print('Congratulations! You got a high score!')
+		for rank, (new_name, new_high_score) in enumerate(hs_list):
+			print(f'{rank}. {new_name} - {new_high_score}')
+
+	config_dict = {}
+	config_dict['high_scores'] = hs_list
+	config_dict['bad_guys'] = badguy_list
+
+	save_dict_to_json(fp, config_dict)
+
+	return hs_list
+
+
+def save_dict_to_json(fp, dic):
+	with open(fp, 'w') as f:
+		json.dump(dic, f, sort_keys=True, indent=4)
+
+
+if __name__ == '__main__':
+	print(TERM.clear)
+
+	player_name = input("What's yer name, traveler? --> ")
+	print("You aren't from around here, are you? This is a gun eat gun town.")
+	print("Are you ready to put your speed to the test? (y/n)")
+
+	with TERM.cbreak():
+		val = ''
+		val = TERM.inkey()
+		if val.lower() != 'y':
+			print("Yer too cowardly to even try? Get outta my bar.")
+			exit()
+
+	print("Good, because here comes someone now to challenge you. The way "
+			+ "to win is by\npressing the displayed letter as fast as you can " 
+			+ "after you see the word 'DRAW'.\nGot it? Good luck. (Press any "
+			+ "key)")
+	
+	config_fp = 'config.json'
+	hs_list, badguy_list = config_loader(config_fp)
+	
+	level = 0
+	score = 0
+
+	press_any_key()
+
+	last_badguy = False
+
+	while True:
+		if len(badguy_list) == level+1:
+				last_badguy = True
+
+		badguy_info = badguy_list[level]
+		print(f"That's {badguy_info[0]}. {badguy_info[2]} But watch out, "
+			+ "they're fast.\n(Press any key) ")
+
+		press_any_key()
+		
+		user_speed = speed_test(badguy_info[1])
+		win = checker(user_speed, badguy_info[1])
+		gunshot_display()
+
+		if win and last_badguy == True:
+			print('Wow! You really are fast! And everyone who lives here is '
+				+ 'dead!')
+			hs_list = high_score_saver(
+				config_fp, hs_list, badguy_list, player_name, score)
+			exit()
+
+		elif win and last_badguy == False:
+			# Blessed press any key thing
+			score += points(user_speed, badguy_info[1], level)
+			print(score)
+			print('Wow! You really are fast! But wait until you see the next '
+				+ 'guy...')
+			
+			level += 1
+
+			press_any_key()
+		elif not win:
+			print('You died.\n')
+			hs_list = high_score_saver(
+				config_fp, hs_list, badguy_list, player_name, score)
+			# blessed y/n
+			print("Want to play again?")
+			with TERM.cbreak():
+				val = ''
+				val = TERM.inkey()
+				if val.lower() != 'y':
+					exit()
